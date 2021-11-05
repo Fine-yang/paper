630e003fcded79eb3313cc1417e0b1e94cd06e61
Yvan Yang <yfnysy@163.com>
2021-11-05 20:24:31 +0800

    add entity&dao

---
+++ demo/src/main/java/com/example/demo/Entity/Pollution.java
@@ -0,0 +1,50 @@
+package com.example.paper.Entity;
+
+public class Pollution {
+    private int id;
+    private String region;
+    private int year;
+    private double cs_pw;
+    private double cs_nw;
+    private double cs_rc_bio;
+    private double cS_ps_bio_sum;
+    private double cs_if_mw_bio;
+    private double cs_ner_mw_bio;
+    private double cs_if_iw;
+    private double cs_ner_iw;
+    private double ae_er_mw;
+    private double ae_if_mw_bio;
+    private double ae_er_iw;
+    private double ae_woode;
+    private double ae_if_iw;
+    private double ce_df;
+    private double cd_wh;
+    private double ce_nwh;
+    private double ce_rpc;
+    private double ce_chem_p;
+    private double ce_wttrt_pulp;
+    private double ce_wttrt_paper;
+    private double ce_wttrt_toNature_pulp;
+    private double ce_wttrt_toNature_paper;
+    private double ce_prd_exbio;
+    private double ce_prd_bio;
+    private double ce_inc_bio;
+    private double ce_er_mw_bio;
+    private double co2_if_mw_bio;
+    private double ce_if_mw_bio;
+    private double cecp_if_mw_bio;
+    private double cesc_if_mw_bio;
+    private double ce_er_iw;
+    private double ce_woode;
+    private double co2_if_iw;
+    private double ce_if_iw;
+    private double cecp_if_iw;
+    private double cesc_if_iw;
+
+
+
+
+
+
+
+}
